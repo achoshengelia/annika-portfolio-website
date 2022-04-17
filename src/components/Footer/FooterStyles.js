b0534d276d7e2e24235543952872ddfd818913a8
gasann95 <a.terwey@rolmail.net>
2022-04-17 20:15:35 +0400

    add footer

---
+++ src/components/Footer/FooterStyles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const MainWrapperStyled = styled.footer`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+`;
+
+export const ListItemsStyled = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 5rem;
+`;
+
+export const ItemStyled = styled.li`
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: ${props => props.theme.colors.text.light};
+  @media ${props => props.theme.breakpoints.sm} {
+    font-size: 1rem;
+  }
+
+  @media ${props => props.theme.breakpoints.xs} {
+    font-size: 0.9rem;
+  }
+`;
+
+export const ItemSocialsStyled = styled.li`
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-left: 3rem;
+  cursor: pointer;
+  color: ${props => props.theme.colors.text.light};
+
+  @media ${props => props.theme.breakpoints.sm} {
+    font-size: 1rem;
+    margin-left: 2rem;
+  }
+
+  @media ${props => props.theme.breakpoints.xs} {
+    font-size: 0.9rem;
+    margin-left: 1.5rem;
+  }
+`;
