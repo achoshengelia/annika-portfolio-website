2fed6d9b2ade1082c86eac3e59e033e8f95d93ce
gasann95 <a.terwey@rolmail.net>
2022-04-20 17:07:51 +0400

    refactor footer

---
+++ src/constants/footer.js
@@ -0,0 +1,19 @@
+import { v4 as createID } from 'uuid';
+
+export const footerItems = [
+  {
+    id: createID(),
+    value: 'Twitter',
+    link: 'https://twitter.com/AnnikaTerwey'
+  },
+  {
+    id: createID(),
+    value: 'Vimeo',
+    link: 'https://vimeo.com/annikaterwey'
+  },
+  {
+    id: createID(),
+    value: 'Instagram',
+    link: 'https://www.instagram.com/dunkleecken/'
+  }
+];
